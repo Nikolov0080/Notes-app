086b703ff602538dad4d43ba6f7597ce426d2953
Nikolov0080 <apofis12343@gmail.com>
2020-08-23 23:32:05 +0300

    created home,login,register pageRoutes

--- notes-app/src/PageRouter.js
+++ notes-app/src/PageRouter.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Pages from './Views/Pages/index';
 
-const App = () => {
+const PageRouter = () => {
   return (
     <div>
       <Router>
         <Switch>
           <Route exact path="/" component={Pages.Home}/>
+          <Route exact path="/login" component={Pages.Login}/>
+          <Route exact path="/register" component={Pages.Register}/>
         </Switch>
       </Router>
     </div>
   );
 }
 
-export default App;
+export default PageRouter;
--- notes-app/src/Views/Pages/index.js
+++ notes-app/src/Views/Pages/index.js
@@ -1,5 +1,10 @@
 import Home from '../Pages/homePage/index';
+import Register from '../Pages/registerPage/index';
+import Login from '../Pages/loginPage/index';
+
 
 export default {
-    Home
+    Home,
+    Login,
+    Register
 }
