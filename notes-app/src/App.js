c5282ad4ebecd44f60e06c3553a3edddc60eece5
Nikolov0080 <apofis12343@gmail.com>
2020-08-23 23:27:28 +0300

    created page-router

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Pages from './Views/Pages/index';
 
 const App = () => {
   return (
     <div>
-   
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Pages.Home}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ notes-app/src/Views/Pages/index.js
@@ -0,0 +1,5 @@
+import Home from '../Pages/homePage/index';
+
+export default {
+    Home
+}
